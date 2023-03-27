1c48080bdbb2388864f0bb5904d05db1d08bb561
kamrote <85530913+kamrote@users.noreply.github.com>
2023-03-26 21:40:47 -0500

    fix folders & add games

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Auto move files on replay buffer
  
 - Uses OBS Advanced scene switcher (https://obsproject.com/forum/resources/advanced-scene-switcher.395/) to move your clips into a folder with the same name as the game your playing (or "desktop" if none are found)
+- Currently supports 1774 games as of 3/26/2023 (any game released after this point will most likely have to be manually added)
 
 ## How to use
 - Download advanced scene switcher from the link above and install it
@@ -9,7 +10,7 @@
 - in advanced scene switcher open it up, make a new macro. add a conditon called "(if) (replay buffer) (saved)" than add a action called "run", put the run file to the save_files.bat file in the repo
 ![Example](https://i.imgur.com/GsuIsrE.png "Example")
 - it should move the file whenever you save a replay buffer
+- (!IMPORTANT) make sure that the place your saving your recordings to in obs is the same place as base_path in processes.json
 
-- (IMPORTANT) make sure that the place your saving your recordings to in obs is the same place as base_path in processes.json
 ## How to add a game
 - open processes.json, and add a new entrie in "processes" (like the others) with the processes name (cap sensetive) and whatever the name for the game is
--- save_files.js
+++ save_files.js
@@ -19,21 +19,9 @@ const getMostRecentFile = (dir) => {
         old: isOld,
     };
 };
-function isRunning(win, mac, linux){
-    return new Promise(function(resolve, reject){
-        const plat = process.platform
-        const cmd = plat == 'win32' ? 'tasklist' : (plat == 'darwin' ? 'ps -ax | grep ' + mac : (plat == 'linux' ? 'ps -A' : ''))
-        const proc = plat == 'win32' ? win : (plat == 'darwin' ? mac : (plat == 'linux' ? linux : ''))
-        if(cmd === '' || proc === ''){
-            resolve(false)
-        }
-        exec(cmd, function(err, stdout, stderr) {
-            resolve(stdout.toLowerCase().indexOf(proc.toLowerCase()) > -1)
-        })
-    })
-}
 const ensureFolderExists = async (folderPath) => {
-    await exec(`mkdir ${path.join(...folderPath.split('/'))}`)
+    console.log(folderPath)
+    await exec(`mkdir "${folderPath.replaceAll('/','\\')}"`);
 };
 async function do_notification(dir_path, game) {
     notifier.notify({
@@ -48,41 +36,51 @@ async function do_notification(dir_path, game) {
     });
 }
 async function check_process(process_name, int) {
-    isRunning(process_name, process_name.split('.exe')[0], process_name.split('.exe')[0]).then(async (v) => {
-        console.log('checking ' + process_name + ' - ' + v) 
-        if (v === true) {
-            let most_recent = getMostRecentFile(process_list.base_path)
-            if (most_recent.old === false) {
-                await ensureFolderExists(process_list.base_path + '/' + process_list.processes[int].game);
-                setTimeout(async () => {
-                    fs.rename(process_list.base_path + '/' + most_recent.file, process_list.base_path + '/' + process_list.processes[int].game + '/' + process_list.processes[int].game + ' ' + most_recent.file, function (err) {
-                        if (err) throw err
-                    })
-                    console.log('found - ' + process_list.processes[int].game)
-                    do_notification(process_list.base_path + '/' + process_list.processes[int].game + '/' + process_list.processes[int].game + ' ' + most_recent.file, process_list.processes[int].game)
-                }, 1500)
-            } else {
-                console.log('too old')
-            }
-        } else {
-            if (process_list.processes[int + 1] !== undefined && process_list.processes[int + 1] !== null) {
-                check_process(process_list.processes[int + 1].process, (int + 1)) //terrible way to do this
-            } else {
+    exec('tasklist', async (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        const lines = stdout.split('\n');
+        const processLines = lines.slice(2);
+        const processNames = processLines.map((line) => {
+            return line.split(/\s+/)[0];
+        });
+        let found_exec = false;
+        for (i = 0; i < processNames.length; i++) {
+            let exec_location = process_list.processes.find(p => p.execs.findIndex(e => e.process.toLowerCase() === processNames[i].toLowerCase()) !== -1)?.game;
+            if (exec_location) {
+                console.log(exec_location + ' is open')
+                found_exec = true
                 let most_recent = getMostRecentFile(process_list.base_path)
                 if (most_recent.old === false) {
-                    await ensureFolderExists(process_list.base_path + '/DESKTOP');
+                    await ensureFolderExists(process_list.base_path + '/' + exec_location);
                     setTimeout(async () => {
-                        fs.rename(process_list.base_path + '/' + most_recent.file, process_list.base_path + '/DESKTOP/DESKTOP ' + most_recent.file, function (err) {
+                        fs.rename(process_list.base_path + '/' + most_recent.file, process_list.base_path + '/' + exec_location + '/' + exec_location + ' ' + most_recent.file, function (err) {
                             if (err) throw err
                         })
-                        do_notification(process_list.base_path + '/DESKTOP/DESKTOP ' + most_recent.file, 'DESKTOP')
-                    }, 1500);
+                        do_notification(process_list.base_path + '/' + exec_location + '/' + exec_location + ' ' + most_recent.file, exec_location)
+                    }, 1500)
                 } else {
-                    console.log('too old')
+                    console.log('file is too old')
                 }
-                console.log('didnt find')
+                break
             }
         }
-    })
+        if (!found_exec) {
+            let most_recent = getMostRecentFile(process_list.base_path)
+            if (most_recent.old === false) {
+                await ensureFolderExists(process_list.base_path + '/DESKTOP');
+                setTimeout(async () => {
+                    fs.rename(process_list.base_path + '/' + most_recent.file, process_list.base_path + '/DESKTOP/DESKTOP ' + most_recent.file, function (err) {
+                        if (err) throw err
+                    })
+                    do_notification(process_list.base_path + '/DESKTOP/DESKTOP ' + most_recent.file, 'DESKTOP')
+                }, 1500);
+            } else {
+                console.log('file is too old')
+            }
+        }
+    });
 }
-check_process(process_list.processes[0].process, 0)
+check_process()
